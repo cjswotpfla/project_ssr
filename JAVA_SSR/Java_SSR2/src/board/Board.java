ca1f7460dcaf825b78687c27f30fe2a3491155e9
SSR <83192206+cjswotpfla@users.noreply.github.com>
2021-05-12 17:29:26 +0900

    자바 12일차 : 게시글 수정

--- JAVA_SSR/Java_SSR2/src/board/Board.java
+++ JAVA_SSR/Java_SSR2/src/board/Board.java
@@ -28,6 +28,7 @@ public class Board {
 	 * 메소드명 : print
 	 * */
 	public void print() {
+		views+=1;
 		System.out.println("번호 : " + num);
 		System.out.println("제목 : " + title);
 		System.out.println("작성자 : " + writer);
@@ -36,7 +37,28 @@ public class Board {
 		System.out.println("작성일 : " + registerDate);
 		System.out.println("내용 : " + contents);
 	}
-	
+	/* 기능 : 게시글 정보를 요약(번호,제목,작성자,작성일,조회수)해서 출력하는 기능
+	 * 매개변수 : x
+	 * 리턴타입 : X => void
+	 * 메소드명 : summaryPrint
+	 * */
+	public void summaryPrint() {
+		System.out.print(num+" ");
+		System.out.print(title+" ");
+		System.out.print(writer+" ");
+		System.out.print(registerDate+" ");
+		System.out.println(views+" ");
+		System.out.println();
+	}
+	/* 기능 : 제목과 내용을 수정하는 메소드
+	 * 매개변수 : 수정할 제목,내용 String title, String contents
+	 * 리턴타입 : 없음 void
+	 * 메소드명 : update
+	 * */
+	public void update(String title, String contents) {
+		this.title=title;
+		this.contents=contents;
+	}
 	public int getNum() {
 		return num;
 	}
