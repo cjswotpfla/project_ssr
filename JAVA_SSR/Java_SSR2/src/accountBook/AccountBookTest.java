8c1e71a626043d53a4679507fec14154fde947f8
SSR <83192206+cjswotpfla@users.noreply.github.com>
2021-05-14 17:32:54 +0900

    자바 14일차 : 가계부 완성!

--- JAVA_SSR/Java_SSR2/src/accountBook/AccountBookTest.java
+++ JAVA_SSR/Java_SSR2/src/accountBook/AccountBookTest.java
@@ -1,17 +1,48 @@
 package accountBook;
 
+import java.util.Scanner;
+
 public class AccountBookTest {
 
 	public static void main(String[] args) {
-		/* 가계부 프로그램 구현하려고 한다. 이 때 필요한 클랴스를 만들어보세요
-		 * 수입 : 수입/지출
-		 * 날짜 : 
-		 * 자산 : 현금/은행/카드
-		 * 분류 : 식비/교통/건강
-		 * 금액 : 
-		 * 내용 : 
-		 * */
-		
+		// 가계부 프로그램 완성본
+
+		AccountBookProgram2 abp = new AccountBookProgram2();
+		int menu=0;
+		Scanner scan= new Scanner(System.in);
+		do {
+			abp.printMenu();
+			menu = scan.nextInt();
+			switch(menu) {
+			case 1:
+				abp.insert();
+				break;
+			case 2:
+				abp.update();
+				break;
+			case 3:
+				abp.delete();
+				break;
+			case 4:
+				abp.printItemListDetail();
+				break;
+			case 5:
+				abp.printItemSimple();
+				break;
+			case 6:
+				abp.printItem();
+				break;
+			case 7:
+				abp.printTotal();
+				break;
+			case 8:
+				abp.clossItem();
+				break;
+			default:
+				System.out.println("잘못된 번호입니다.");
+			}
+		}while(menu!=8);
+	scan.close();
 	}
 
 }
