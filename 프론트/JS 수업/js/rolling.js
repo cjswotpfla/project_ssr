94eb669a7a6c252053b4fd039fedda1eeab9ae7a
SSR <83192206+cjswotpfla@users.noreply.github.com>
2021-06-25 17:34:11 +0900

    JS 7일차 : rolling JS파일

--- 프론트/JS 수업/js/rolling.js
+++ 프론트/JS 수업/js/rolling.js
@@ -12,7 +12,7 @@ function rollingTop(list, listItem, marginTop, animateTime, intervalTime){
 
 function rollingLeft(list, listItem, animateTime, intervalTime){
 	var id = setInterval(function(){
-		if($(list +' ' + listItem).first()){
+		if(!$(list +' ' + listItem).first().is(':animated')){
 			var width = $(list +' ' + listItem).first().width();
 			$(list +' ' + listItem).first()
 				.animate({'margin-left':'-'+width}, animateTime,function(){
@@ -21,4 +21,25 @@ function rollingLeft(list, listItem, animateTime, intervalTime){
 		}
 	},intervalTime);
 	return id;
+}
+function rollingRight(list, listItem, animateTime, intervalTime){
+	var id = 	setInterval(function(){
+		if(!$(list + ' ' + listItem).first().is(':animated')){
+			var width = $(list + ' ' + listItem).last().width();
+			$(list + ' ' + listItem).last().detach().prependTo(list)
+				.css('margin-left',-width+'px')
+				.animate({'margin-left':'0px'},animateTime)
+		}
+	},intervalTime);
+	return id; 
+}
+function rolling(direction, list, listItem,marginTop, animateTime, intervalTime){
+	var id;
+	if(direction.toLowerCase() == 'left'){
+		return rollingLeft(list,listItem,animateTime,intervalTime);
+	}else if(direction.toLowerCase()=='right'){
+		return rollingRight(list,listItem,animateTime,intervalTime);
+	}else{
+		return rollingTop(list,listItem,margin,animateTime,intervalTime);
+	}
 }
