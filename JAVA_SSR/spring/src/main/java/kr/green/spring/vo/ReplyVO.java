b39d74d46b0545d84556b0884972ae901d48c959
SSR <83192206+cjswotpfla@users.noreply.github.com>
2021-07-20 17:30:19 +0900

    SPRING : ajax 연습

---
+++ JAVA_SSR/spring/src/main/java/kr/green/spring/vo/ReplyVO.java
@@ -0,0 +1,12 @@
+package kr.green.spring.vo;
+
+import lombok.*;
+@Data
+@NoArgsConstructor
+public class ReplyVO {
+	private int rp_num;
+	private int rp_bd_num;
+	private String rp_me_id;
+	private String rp_content;
+	private String rp_valid;
+}
