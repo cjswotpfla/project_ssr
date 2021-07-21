7327b6e596073915799e85e97b266825970b3dc7
SSR <83192206+cjswotpfla@users.noreply.github.com>
2021-07-21 11:46:13 +0900

    SPRING : 댓글 등록 및 확인 기능 추가

---
+++ JAVA_SSR/spring/src/main/java/kr/green/spring/service/ReplyService.java
@@ -0,0 +1,16 @@
+package kr.green.spring.service;
+
+import java.util.ArrayList;
+
+import kr.green.spring.vo.ReplyVO;
+
+public interface ReplyService {
+
+	void insertReply(ReplyVO reply);
+
+	ArrayList<ReplyVO> getReplyList(Integer num);
+
+
+
+
+}
